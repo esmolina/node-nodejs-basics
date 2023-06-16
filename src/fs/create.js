a6815faad01e6c903f6b3c48d99247e724e276e8
esmolina <elenasmolinann@gmail.com>
2023-06-16 22:17:44 +0300

    refactor: change imports and formatting in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,7 +1,8 @@
 import { stat, writeFile } from 'fs/promises';
 import path from 'node:path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const filePath = path.join(__dirname, 'files', 'fresh.txt');
