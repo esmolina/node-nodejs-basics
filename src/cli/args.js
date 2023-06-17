dadf6ece58671bdeb201e397dccaffc2ad13ff80
esmolina <elenasmolinann@gmail.com>
2023-06-17 19:49:29 +0300

    feat: solve args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const consoleMessageArray = [];
+  for (let i = 2; i <= argv.length - 1; i=i+2) {
+    const varName = (`${argv[i]}`).slice(2);
+    const varContent = `${argv[i+1]}`;
+    consoleMessageArray.push(`${varName} is ${varContent}`);
+  }
+  const consoleMessageString = consoleMessageArray.join(', ');
+  console.log(consoleMessageString);
 };
 
 parseArgs();
