081f0feafb312cf83bdbd20f31933b69c6a5ce56
esmolina <elenasmolinann@gmail.com>
2023-06-17 16:42:41 +0300

    feat: solve read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,28 @@
+import { stat, readFile } from 'fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import {stdout} from 'node:process'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+const errorMessage = 'FS operation failed';
+const outputChannel = stdout;
+
 const read = async () => {
-    // Write your code here 
+  try {
+    await stat(filePath);
+    throw new Error(errorMessage);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(errorMessage);
+    } else {
+      const fileContent = await readFile(filePath);
+      outputChannel.write(`${fileContent}\n`);
+    }
+  }
+
 };
 
 await read();
