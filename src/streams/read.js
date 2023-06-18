0050dc8f343f43468387270b9ec014540fb0a659
esmolina <elenasmolinann@gmail.com>
2023-06-18 11:27:30 +0300

    feat: solve streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+const outputChannel = stdout;
+const errorMessage = 'Something wrong';
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath);
+  readStream.on('data', (fileContent) => {
+    outputChannel.write(`${fileContent}\n`);
+  });
+
+  readStream.on('error', (error) => {
+    outputChannel.write(errorMessage);
+  });
 };
 
 await read();
