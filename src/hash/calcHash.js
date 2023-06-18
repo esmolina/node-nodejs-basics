2c7e2e6965c702279838d074ada3a44ddf560cce
esmolina <elenasmolinann@gmail.com>
2023-06-18 10:58:54 +0300

    feat: solve calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const data = await createReadStream(filePath);
+  const hash = await createHash('sha256');
+  data.pipe(hash).setEncoding('hex').on('data', (chunk) => {
+    console.log(chunk);
+  });
 };
 
 await calculateHash();
