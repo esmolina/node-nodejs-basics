8c52d098c49e4a72888a0e68e4e4d4993a3a106b
esmolina <elenasmolinann@gmail.com>
2023-06-18 23:14:49 +0300

    refactor: add promises in wt/main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -11,33 +11,36 @@ const workerFilePath = path.join(__dirname, 'worker.js');
 const performCalculations = async () => {
   const numberOfCores = cpus().length;
   const resultsObject = {};
+  const promises = [];
 
   for (let i = 0; i < numberOfCores; i++) {
     const data = 10 + i;
     const worker = new Worker(workerFilePath, {workerData: { data }});
 
-    worker.on('message', msg => {
-      resultsObject[i] = { status: 'resolved', data: msg };
-      if (Object.keys(resultsObject).length === numberOfCores) {
-        const resultsArray = [];
-        for (let j = 0; j < numberOfCores; j++) {
-          resultsArray.push(resultsObject[j]);
-        }
-        console.log(resultsArray);
-      }
+    const promise = new Promise((resolve, reject) => {
+      worker.on('message', msg => {
+        resultsObject[i] = { status: 'resolved', data: msg };
+        resolve();
+      });
+
+      worker.on('error', error => {
+        resultsObject[i] = { status: 'error', data: null };
+        resolve();
+      });
     });
 
-    worker.on('error', error => {
-      resultsObject[i] = { status: 'error', data: null };
-      if (Object.keys(resultsObject).length === numberOfCores) {
-        const resultsArray = [];
-        for (let j = 0; j < numberOfCores; j++) {
-          resultsArray.push(resultsObject[j]);
-        }
-        console.log(resultsArray);
-      }
-    });
+    promises.push(promise);
   }
+
+  await Promise.all(promises);
+
+  const resultsArray = [];
+
+  for (let j = 0; j < numberOfCores; j++) {
+    resultsArray.push(resultsObject[j]);
+  }
+
+  console.log(resultsArray);
 };
 
 await performCalculations();
