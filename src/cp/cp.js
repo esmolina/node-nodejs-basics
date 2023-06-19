85aa0a942b2250faba645e1f3359a6f891041ba5
esmolina <elenasmolinann@gmail.com>
2023-06-19 10:32:51 +0300

    refactor: add comments for cp module

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -17,3 +17,14 @@ const spawnChildProcess = async (args) => {
 };
 
 spawnChildProcess(  [ 'hello', 'dear', 'friend'] );
+
+
+/* comment for cross-check
+spawnChildProcess create child-process in spawn-method (node - is the command that runs the script, meaning node script.js)
+stdio-option take info how data will be transferred between the parent process and child process (IPC (Inter-Process Communication))
+stdio: [mainStdin, 'pipe', mainStdout] means that the input stream of the parent process (main Stdin)
+will be the input stream of the child process, and the output stream of the child process will be the
+output stream of the parent process (mainStdout)
+
+child procces receive data from parent process in childProcess.stdout.pipe(mainStdout);
+ */
